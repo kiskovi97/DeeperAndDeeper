ea8d7953827ff25ac983746ff48c2610bc2ddb2b
kiskovi97 <kiskovi97@gmail.com>
2020-06-16 19:43:12 +0200

    Skin Selector Basic

---
+++ Assets/Scripts/MarketPlace/SkinList.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkinList : MonoBehaviour
+{
+    public CharacterSkin[] skins;
+
+    public GameObject skinObjectPrefab;
+
+    private RectTransform rectTransform;
+
+    private float width = 214;
+
+    private HorizontalLayoutGroup horizontalLayoutGroup;
+
+    private bool changed = true;
+
+    public void SetItemsForSale(CharacterSkin[] items)
+    {
+        skins = items;
+        changed = true;
+    }
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        horizontalLayoutGroup = GetComponent<HorizontalLayoutGroup>();
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        if (!changed) return;
+        changed = false;
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject, 0.1f);
+        }
+
+        for (int i = 0; i < skins.Length; i++)
+        {
+            var obj = Instantiate(skinObjectPrefab, transform);
+            var item = obj.GetComponent<SkinObject>();
+            item.skin = skins[i];
+
+        }
+    }
+}
---
+++ Assets/Scripts/MarketPlace/SkinObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkinObject : MonoBehaviour
+{
+    public CharacterSkin skin;
+
+    public Image image;
+
+    // Start is called before the first frame update
+    void Update()
+    {
+        if (skin != null)
+            image.sprite = skin.Image;
+
+
+        var rect = GetComponent<RectTransform>();
+        
+        rect.sizeDelta = new Vector2(rect.rect.height, rect.rect.height);
+    }
+}
