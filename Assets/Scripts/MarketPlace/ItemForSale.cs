bd09fb67c2406fb873133e8bb66720f208c71bbe
kiskovi97 <kiskovi97@gmail.com>
2020-06-16 13:06:33 +0200

    Item for sale

---
+++ Assets/Scripts/MarketPlace/ItemForSale.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Item For Sale", menuName = "MarketPlace/ItemForSale")]
+public class ItemForSale : ScriptableObject
+{
+    public string name;
+
+    public int price;
+
+    public Sprite image;
+
+    public string desciption;
+}
---
+++ Assets/Scripts/MarketPlace/ItemForSaleObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemForSaleObject : MonoBehaviour
+{
+    public ItemForSale item;
+
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI descriptionText;
+    public TextMeshProUGUI priceText;
+
+    public Image image;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (item == null) return;
+
+        nameText.text = item.name;
+        descriptionText.text = item.desciption;
+        priceText.text = item.price.ToString();
+
+        image.sprite = item.image;
+    }
+}
