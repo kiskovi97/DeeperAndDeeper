757f3f7f45f810bf37aecff32caa106aa5c4d5b3
kiskovi97 <kiskovi97@gmail.com>
2020-06-26 16:08:52 +0200

    Remove Facebook Dependecy

--- Assets/Scripts/FacebookProxy.cs
+++ Assets/Scripts/FacebookProxy.cs
@@ -1,33 +1,12 @@
 ﻿using UnityEngine;
-using Facebook.Unity;
 using System.Collections;
 using System.IO;
 
 public class FacebookProxy : MonoBehaviour
 {
-    private void Awake()
-    {
-        if (!FB.IsInitialized)
-        {
-            FB.Init();
-        }
-        else
-        {
-            FB.ActivateApp();
-        }
-    }
     public void Share()
     {
         SocialShare();
-    }
-
-    public void FacebookShare()
-    {
-        FB.ShareLink(
-             contentTitle: "Deeper and Deeper",
-             contentDescription: "It's a fun game, when the player must go depper and deeper into the caves.",
-             contentURL: new System.Uri("https://play.google.com/store/apps/details?id=com.igorodcavok.DeeperAndDeeper"),
-             callback: Callback);
     }
 
     public void SocialShare()
@@ -36,13 +15,5 @@ public class FacebookProxy : MonoBehaviour
            .SetText("I just played with deeper and deeper! "
            +"My score was: "+GameState.score+" https://play.google.com/store/apps/details?id=com.igorodcavok.DeeperAndDeeper")
            .Share();
-    }
-
-    void Callback(IShareResult shareResult)
-    {
-        if (shareResult.Cancelled)
-        {
-            Debug.Log(shareResult.RawResult);
-        }
     }
 }
